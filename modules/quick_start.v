d8528ea01f02aa671da6c0024e393874c04dc9a8
micherciu <cherciumadalin@gmail.com>
2025-04-10 15:15:22 +0300

    [update] some changes to the variables definition: quick_start.v

--- modules/quick_start.v
+++ modules/quick_start.v
@@ -6,8 +6,8 @@ module quick_start(input logic clk);
   timeunit 1ns;
   timeprecision 1ns;
 
-  output logic my_signal_1;
-  output logic my_signal_2;
+  output my_signal_1;
+  output my_signal_2;
 
   assign my_signal_1 = 1'b0;
   assign my_signal_2 = 1'b0;
