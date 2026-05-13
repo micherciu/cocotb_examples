9b313eca389623f3d57bde7ce8627652c0efb80a
micherciu <cherciumadalin@gmail.com>
2026-05-13 14:46:33 +0300

    [M] signal 1 high, signal 2 low ...

--- modules/quick_start.v
+++ modules/quick_start.v
@@ -9,7 +9,7 @@ module quick_start(input logic clk);
   output my_signal_1;
   output my_signal_2;
 
-  assign my_signal_1 = 1'b0;
+  assign my_signal_1 = 1'b1;
   assign my_signal_2 = 1'b0;
 
 endmodule
