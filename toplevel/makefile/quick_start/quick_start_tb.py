eab7dc446974a369b6b83f5059c9c155a0b7beeb
micherciu <cherciumadalin@gmail.com>
2026-05-13 12:40:24 +0300

    [M] small changes to the timming  ...

--- toplevel/makefile/quick_start/quick_start_tb.py
+++ toplevel/makefile/quick_start/quick_start_tb.py
@@ -3,15 +3,15 @@
 # quick_start_tb.py
 
 import cocotb
-from cocotb.triggers import FallingEdge, Timer
+from cocotb.triggers import RisingEdge, Timer
 
 async def generate_clock(dut):
     """Generate clock pulses."""
 
     for cycle in range(10):
-        dut.clk.value = 0
-        await Timer(1, unit="ns")
         dut.clk.value = 1
+        await Timer(1, unit="ns")
+        dut.clk.value = 0
         await Timer(1, unit="ns")
 
 
@@ -21,20 +21,20 @@ async def quick_start(dut):
 
     await cocotb.start(generate_clock(dut))  # run the clock "in the background"
 
-    await Timer(5, unit="ns")  # wait a bit
-    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
+    await Timer(4, unit="ns")  # wait a bit
+    await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
 
     dut._log.info("my_signal_1 is %s", dut.my_signal_1.value)
     dut.my_signal_1.value = 1
 
-    await Timer(5, unit="ns")  # wait a bit
-    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
+    await Timer(4, unit="ns")  # wait a bit
+    await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
 
     dut._log.info("my_signal_1 is %s", dut.my_signal_1.value)
     dut.my_signal_2.value = 1
     dut.my_signal_1.value = 0
 
-    await Timer(5, unit="ns")  # wait a bit
-    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
+    await Timer(4, unit="ns")  # wait a bit
+    await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
     dut.my_signal_2.value = 0
     dut.my_signal_1.value = 0
